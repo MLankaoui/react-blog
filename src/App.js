ba50234d005a1d6f2fab40a1d2347997bc0fb861
MLankaoui <marouanelankaoui66@gmail.com>
2024-07-23 12:37:20 +0100

    add navbar items and add the navbar to the page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 export default function App() {
+
   return (
     <div className="App">
+      <Navbar />
       <div className='content'>
-        <h1> App component </h1>
+        <h1> App content </h1>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <>
-    </>
+    <nav className='navbar'>
+        <h1>
+            The React Blog
+        </h1>
+
+        <div className='links'>
+            <a href='/'>Home</a>
+            <a href='/create'>New Blog</a>
+        </div>
+    </nav>
   )
 }
 
