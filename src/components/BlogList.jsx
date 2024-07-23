336c6e874fe35c098e383d5467259dec7c50798e
MLankaoui <marouanelankaoui66@gmail.com>
2024-07-23 14:39:30 +0100

    pass all the dummy to the specific blogposts component and add a title, and debug

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Bloglist() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function Bloglist({data, title}) {
+    
+    return (
+        <div className='bog-list'>
+            <h2>{title}</h2>
+            {data.map((post) => (
+                <div className='blog-preview' key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p> Written by: {post.author} </p>
+                </div>
+
+            ))}
+        </div>
+    )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,21 @@
 import React, { useState } from 'react'
+import BlogList from './BlogList';
 
 export default function Home() {
     // some dummy data
-
     const blogpost = [
         { title: 'My new website', body: 'lorem ipsum...', author: 'Marouane', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Fatima', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Marouane', id: 3 }
-      ]
-    const [blogs, setBlogs] = useState(blogpost)
+    ]
+    const [blogs, setBlogs] = useState(blogpost);
     return (
         <div className='home'>
-        <h1> Home Page </h1>
-        {blogs.map((post) => (
-            <div className='blog-preview' key={post.id}>
-                <h2>{post.title}</h2>
-                <p> Written by: {post.author} </p>
-            </div>
-
-        ))}
+            <h1> Home Page </h1>
+            <br />
+            <br />
+            <br />
+            <BlogList data={blogs} title="All blogs!" />
         </div>
     );
 }
