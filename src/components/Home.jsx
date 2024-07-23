6b1333310e38d3af98c3459ce3eae3317273f8bd
MLankaoui <marouanelankaoui66@gmail.com>
2024-07-23 13:40:14 +0100

    add dummy articles data to home page.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Home() {
-  return (
-    <div className='home'>
-      <h1> Home Page </h1>
-    </div>
-  )
+    // some dummy data
+
+    const blogpost = [
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ]
+    const [blogs, setBlogs] = useState(blogpost)
+    return (
+        <div className='home'>
+        <h1> Home Page </h1>
+        {blogs.map((post) => (
+            <div className='blog-preview' key={post.id}>
+                <h2>{post.title}</h2>
+                <p> {post.author} </p>
+            </div>
+
+        ))}
+        </div>
+    );
 }
 
 
